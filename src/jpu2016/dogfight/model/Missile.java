1675e5ef8f8998acd41f9d2411839c74ffdbb5fd
ClementFbt <clement.froz@gmail.com>
2017-06-02 13:26:25 +0200

    PULL

--- src/jpu2016/dogfight/model/Missile.java
+++ src/jpu2016/dogfight/model/Missile.java
@@ -1,32 +1,36 @@
 package jpu2016.dogfight.model;
 
-public class Missile extends Mobile{
-	private static int SPEED = 4;
-	private static int WIDTH = 30;
-	private static int HEIGHT = 10;
-	private static int MAX_DISTANCE_TRAVELED = 1400;
-	private static String IMAGE = "missile";
-	private final int distanceTraveled = 0;
-	//test
+public class Missile extends Mobile {
+	private static int		SPEED					= 4;
+	private static int		WIDTH					= 30;
+	private static int		HEIGHT					= 10;
+	private static int		MAX_DISTANCE_TRAVELED	= 1400;
+	private static String	IMAGE					= "missile";
+	private final int		distanceTraveled		= 0;
+	// test
 
-	public Missile(final Direction direction, final Position position){
-		super(direction, position, new Dimension(WIDTH, HEIGHT), SPEED, IMAGE);
+	public Missile(final Direction direction, final Position position) {
+		super(direction, position, new Dimension(Missile.WIDTH, Missile.HEIGHT), Missile.SPEED, Missile.IMAGE);
 	}
-	public int getWidthWithADirection(final Direction direction){
+
+	public int getWidthWithADirection(final Direction direction) {
 		return this.distanceTraveled;
 
 	}
-	public int getHeightWithADirection(final Direction direction){
+
+	public int getHeightWithADirection(final Direction direction) {
 		return this.distanceTraveled;
 
 	}
+
 	@Override
-	public void move(){
+	public void move() {
 
 	}
+
 	@Override
-	public boolean isWeapon(){
-		return false;
+	public boolean isWeapon() {
+		return true;
 
 	}
 }
