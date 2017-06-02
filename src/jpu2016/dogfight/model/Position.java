e8e1f07150eafd965b668bcb71d3b21121a83fb9
BaptisteLauras <baptiste.lauras@viacesi.fr>
2017-06-02 13:33:37 +0200

    jai fait des egals mdr

--- src/jpu2016/dogfight/model/Position.java
+++ src/jpu2016/dogfight/model/Position.java
@@ -7,11 +7,17 @@ public class Position {
 	double maxY;
 
 	public Position(final double x, final double y, final double maxX, final double maxY){
-
+		this.x = x;
+		this.y = y;
+		this.maxX = maxX;
+		this.maxY = maxY;
 	}
 
 	public Position(final Position position){
-
+		this.x = position.x;
+		this.y = position.y;
+		this.maxX = position.maxX;
+		this.maxY = position.maxY;
 	}
 
 	public double getX() {
