3f7c497a7fce86ab58156f7c8bceead2424a0f09
ClementFbt <clement.froz@gmail.com>
2017-06-02 10:24:26 +0200

    phase3

--- src/jpu2016/dogfight/view/EventPerformer.java
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -1,16 +1,29 @@
 package jpu2016.dogfight.view;
 
 import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.controller.UserOrder;
 
-public class EventPerformer {
+public class EventPerformer implements IOrderPerformer {
 	public int keyCode;
-	public EventPerformer(IOrderPerformer orderPerformer){
 
+	private IP
+	private UserOrder
+
+	public EventPerformer(IOrderPerformer orderPerformer) {
+		this.orderPerformer = ;
 	}
+
 	public void eventPerform() {
 	}
+
 	private int keyCodeToUserOrder() {
 		return UserOrder;
+	}
+
+	@Override
+	public void orderPerform(UserOrder userOrder) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
