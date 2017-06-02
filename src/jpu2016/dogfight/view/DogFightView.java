62dc63a7c5e83d735b1a1558da234876e8817915
SuperNiac <totodu33290@gmail.com>
2017-06-02 09:42:42 +0200

    phase 3

---
+++ src/jpu2016/dogfight/view/DogFightView.java
@@ -0,0 +1,16 @@
+package jpu2016.dogfight.view;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+
+public class DogFightView {
+	public String Message;
+	public DogFightView(IOrderPerformer orderPerformer,IDogfightModel dogfightModel,Observable observable){
+
+	}
+	public String displayMessage(){
+		return this.Message;
+	}
+	private void run(){
+
+	}
+}
---
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,16 @@
+package jpu2016.dogfight.view;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+
+public class EventPerformer {
+	public int keyCode;
+	public EventPerformer(IOrderPerformer orderPerformer){
+
+	}
+	public void eventPerform() {
+	}
+	private int keyCodeToUserOrder() {
+		return UserOrder;
+	}
+
+}
