17bcdef8addabfc521db89a4496f7e22c5a7fbcf
ClementFbt <clement.froz@gmail.com>
2017-06-02 11:07:06 +0200

    phase4

--- src/jpu2016/dogfight/gameframe/GameFrame.java
+++ src/jpu2016/dogfight/gameframe/GameFrame.java
@@ -1,5 +1,41 @@
 package jpu2016.dogfight.gameframe;
 
-public class GameFrame {
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame implements IEventPerformer {
+
+	/**
+	 *
+	 */
+	private static final long		serialVersionUID	= -9003193788598716910L;
+	private IGraphicsBuilder		graphicBuilder;
+	private final IEventPerformer	eventPerformer;
+	public static String			title				= " ";
+
+	public GameFrame(String title, IEventPerformer eventPerformer, IGraphicsBuilder graphicBuilder,
+			Observable observable) {
+		this.eventPerformer = eventPerformer;
+	}
+
+	public void keyPressed(KeyEvent keyEvent) {
+
+	}
+
+	public void keyRelease(KeyEvent keyEvent) {
+
+	}
+
+	public void keyTyped(KeyEvent keyEvent) {
+
+	}
+
+	@Override
+	public void eventPerform(KeyEvent keyCode) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- src/jpu2016/dogfight/gameframe/GamePanel.java
+++ src/jpu2016/dogfight/gameframe/GamePanel.java
@@ -1,5 +1,32 @@
 package jpu2016.dogfight.gameframe;
 
-public class GamePanel {
+import java.awt.Graphics;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Observer {
+
+	private final IGraphicsBuilder graphicsBuilder;
+
+	public GamePanel(IGraphicsBuilder graphicsBuilder) {
+		this.graphicsBuilder = graphicsBuilder;
+	}
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1521514564856159726L;
+
+	@Override
+	protected void paintComponent(final Graphics graphics) {
+		this.graphicsBuilder.applyModelToGraphics(graphics, this);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		this.repaint();
+	}
 
 }
